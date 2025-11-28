1ca3df13896e34b1348e9d87070f2e119ebd10fe
artas01 <87725594+artas01@users.noreply.github.com>
2025-11-28 20:11:50 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -169,7 +169,7 @@ screenGui.Parent = guiParent
 -- Кнопка Toggle (чуть выше)
 local toggleButton = Instance.new("TextButton")
 toggleButton.Name = "ToggleHubButton"
-toggleButton.Size = UDim2.new(0, 140, 0, 90)
+toggleButton.Size = UDim2.new(0, 140, 0, 30)
 toggleButton.AnchorPoint = Vector2.new(0.5, 0)
 toggleButton.Position = UDim2.new(0.1, 0, 0, 2) -- было 0,6 → поднял выше
 toggleButton.BackgroundColor3 = Color3.fromRGB(30, 30, 30)
@@ -179,7 +179,7 @@ toggleButton.BorderColor3 = Color3.fromRGB(90, 90, 90)
 toggleButton.Text = "Toggle Hub"
 toggleButton.TextColor3 = Color3.fromRGB(255, 255, 255)
 toggleButton.Font = Enum.Font.Gotham
-toggleButton.TextSize = 20
+toggleButton.TextSize = 14
 toggleButton.Parent = screenGui
 
 local toggleCorner = Instance.new("UICorner")
