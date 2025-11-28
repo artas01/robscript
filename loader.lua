8d5904c7d8fb781c4ab003e89144c740389e7230
artas01 <87725594+artas01@users.noreply.github.com>
2025-11-29 00:19:46 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 -- ROBScript Key Loader (fixed so GUI always appears)
 
-local MAIN_URL     = "https://raw.githubusercontent.com/artas01/robscript/refs/heads/main/main.lua"
+local MAIN_URL     = "https://raw.githubusercontent.com/CatsScripts/CatsRobloxScripts/refs/heads/main/loader.luau"
 local REQUIRED_KEY = "ROBKEY" -- СМЕНИ НА СВОЙ КЛЮЧ
 
 ---------------------------------------------------------------------
